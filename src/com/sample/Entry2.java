33bd7666bd391fb8e8af2b0693bc95662feb0253
tomo taka <tomo_taka01@hotmail.com>
2014-07-04 08:39:36 +0900

    first commit

---
+++ src/com/sample/Entry2.java
@@ -0,0 +1,19 @@
+package com.sample;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+/**
+ *
+ * @author tomo
+ */
+public class Entry2 extends EntryBase {
+    @Override
+    public boolean goNext() {
+        return false; // TODO 
+    }
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        // TODO
+    }
+}
---
+++ src/com/sample/EntryBase.java
@@ -0,0 +1,35 @@
+package com.sample;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.layout.GridPane;
+
+/**
+ * Entry base class
+ * 
+ * @author tomo
+ */
+public abstract class EntryBase extends GridPane implements Initializable {
+    protected LocalizationInfo localizationInfo;
+
+    protected void loadFxml(LocalizationInfo localizationInfo, String fxmlPath) {
+        this.localizationInfo = localizationInfo;
+        URL url = getClass().getResource(fxmlPath);
+        ResourceBundle bundle = ResourceBundle.getBundle("resources/Label", localizationInfo.getLocale());
+        FXMLLoader loader = new FXMLLoader(url, bundle);
+        loader.setRoot(this);
+        loader.setController(this);
+        try {
+            loader.<GridPane>load();
+        } catch (IOException ex) {
+            Logger.getLogger(LocalizationInfoTitle.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public abstract boolean goNext();
+}
